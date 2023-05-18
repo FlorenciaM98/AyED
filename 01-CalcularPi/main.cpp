86dce8bb49ae04a185166bc3686dd6b622b4bf9d
Florencia Molle <floorimolle98@gmail.com>
2023-05-18 09:02:47 -0300

    Primer commit

---
+++ 01-CalcularPi/main.cpp
@@ -0,0 +1,4 @@
+//TP1 individual de algoritmos, están las consignas en el aula virtual
+//están todas las indicaciones en el mismo tp en el aula
+//fecha limite 2 de junio como primera entrega, puedo entregar algo que se parece
+//fecha limite de segunda entrega 23 de junio, esta entrega es para rehacer o terminar lo que entegué en la primera fecha
