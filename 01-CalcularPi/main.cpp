ac08fc82b993acce3c6a85372ae3b8488a2e0f43
Florencia Molle <floorimolle98@gmail.com>
2023-05-28 17:53:01 -0300

    Trabajo terminado

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -4,9 +4,22 @@
 //fecha limite de segunda entrega 23 de junio, esta entrega es para rehacer o terminar lo que entegué en la primera fecha
 
 #include <iostream>
+#include <fstream>
+#include <cmath>
+
 using namespace std;
 
-int main() {
 
-    cout << "hello world" << endl;
+
+int main() {
+    float c = 0;
+    int i;
+
+    for (i = 0; i <= 1000000; i++){
+        c = (pow((-1), i) / ((2 * i) + 1)) + c;
+    }
+
+    cout.precision(7);
+
+    cout << (c*4) << endl;
 }
