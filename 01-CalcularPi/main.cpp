026e4585f32c9f3f5240c1d2288c41943e40f108
Florencia Molle <floorimolle98@gmail.com>
2023-05-28 17:58:36 -0300

    eliminacion de anotaciones innecesarias

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,8 +1,3 @@
-//TP1 individual de algoritmos, están las consignas en el aula virtual
-//están todas las indicaciones en el mismo tp en el aula
-//fecha limite 2 de junio como primera entrega, puedo entregar algo que se parece
-//fecha limite de segunda entrega 23 de junio, esta entrega es para rehacer o terminar lo que entegué en la primera fecha
-
 #include <iostream>
 #include <fstream>
 #include <cmath>
