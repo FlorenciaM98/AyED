9e288c038b2e0fbffe93dd0640e879ce4818fabd
Florencia Molle <floorimolle98@gmail.com>
2023-09-19 20:32:55 -0300

    Correccion del tp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -7,14 +7,24 @@ using namespace std;
 
 
 int main() {
-    float c = 0;
-    int i;
+    float c = 0.0;
+    int i = 0;
+    float pi = 3.141592;
 
-    for (i = 0; i <= 1000000; i++){
-        c = (pow((-1), i) / ((2 * i) + 1)) + c;
+    while( abs(c) != (pi/4)) {
+
+        c = (pow((-1), i) / ((2.0 * i) + 1)) + c;
+        i++;
+        cout << c << endl;
+
     }
+
+    /* for (i = 0; i <= 1000000; i++){
+        
+    } */
 
     cout.precision(7);
 
     cout << (c*4) << endl;
+    
 }
